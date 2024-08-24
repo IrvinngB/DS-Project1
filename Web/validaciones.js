d7463d7676dfcdcb95e9d527ae52bc7c703850de
IrvinngB <Irvin.benitez@utp.ac.pa>
2024-08-23 23:50:19 -0500

    Se agrego validacion

--- Web/validaciones.js
+++ Web/validaciones.js
@@ -1,5 +1,5 @@
 // Función para limitar la cantidad de caracteres y permitir solo números
-function limitarCaracteresYNumeros(input, maxLength) {
+function limitarC(input, maxLength) {
     input.addEventListener('input', function() {
         // Remueve todos los caracteres que no sean números
         this.value = this.value.replace(/\D/g, '');
@@ -16,6 +16,35 @@ document.addEventListener('DOMContentLoaded', function() {
     const tomoInput = document.getElementById('tomo');
     const asientoInput = document.getElementById('asiento');
 
-    limitarCaracteresYNumeros(tomoInput, 5);  // Limitar a 10 caracteres
-    limitarCaracteresYNumeros(asientoInput, 5);  // Limitar a 10 caracteres
+    limitarC(tomoInput, 5);  
+    limitarC(asientoInput, 5);  
+    lim
 });
+
+
+function ValidadNombres(input, maxLength) {
+    input.addEventListener('input', function() {
+        // Remueve todos los caracteres que no sean números
+        this.value = this.value.replace(/\d/g, '');
+
+        // Limita la longitud del campo al valor especificado
+        if (this.value.length > maxLength) {
+            this.value = this.value.slice(0, maxLength);
+        }
+    });
+}
+
+// Aplicar la función a los campos que necesiten la validación
+document.addEventListener('DOMContentLoaded', function() {
+    const nombre1Input = document.getElementById('nombre1');
+    const apellido1Input = document.getElementById('apellido1');
+    const nombre2Input = document.getElementById('nombre2');
+    const apellido2Input = document.getElementById('apellido2');
+
+    ValidadNombres(nombre1Input, 30);  
+    ValidadNombres(apellido1Input, 30);  
+    ValidadNombres(nombre2Input, 30);  
+    ValidadNombres(apellido2Input, 30);  
+});
+
+
